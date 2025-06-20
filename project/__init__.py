8cadfd16dd0589f7890082a80958ff6dd6c959f4
YuraDnistriansky <yura.dniester@gmail.com>
2025-06-20 18:34:54 +0300

    add db

---
+++ project/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_utils import database_exists, create_database
+from my import db_password
+
+db = SQLAlchemy()
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    init_db(app)
+    return app
+
+def init_db(app: Flask) -> None:
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://root:{db_password}@127.0.0.1:3306/WeatherStation"
+
+    if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
+        create_database(app.config["SQLALCHEMY_DATABASE_URI"])
+        print("Create database")
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+        print("Create tables")
