e2964600ae52e623e01c2ab2381dab5a1051b820
YuraDnistriansky <yura.dniester@gmail.com>
2025-07-28 16:48:59 +0300

    update service and controller level user

--- project/ORM/controller/orders/user_controller.py
+++ project/ORM/controller/orders/user_controller.py
@@ -1,7 +1,19 @@
+from flask import abort
+from http import HTTPStatus
 from project.ORM.controller.general_controller import GeneralController
 from project.ORM.service import user_service
 from project.ORM.domain.orders.user import User
+from project.Exceptions.UserIsExistsException import UserIsExistException
 
 class UserController(GeneralController):
     _service = user_service
     _class_type = User
+
+    def add(self, obj: _class_type) -> None:
+        if not obj:
+            abort(HTTPStatus.UNPROCESSABLE_ENTITY)
+        try:
+            self._service.add(obj)
+        except UserIsExistException as ex:
+            self._logger.warning(ex)
+            abort(HTTPStatus.CONFLICT)
--- project/ORM/service/orders/user_service.py
+++ project/ORM/service/orders/user_service.py
@@ -1,7 +1,15 @@
 from project.ORM.service.general_service import GeneralService
 from project.ORM.dao import user_dao
 from project.ORM.domain.orders.user import User
+from project.Exceptions.UserIsExistsException import UserIsExistException
 
 class UserService(GeneralService):
     _dao = user_dao
     _class_type = User
+
+    def add(self, obj: _class_type) -> None:
+        all_user = self._session.query(User).all()
+        for user in all_user:
+            if user.email == obj.email:
+                raise UserIsExistException(f"User already exists (email == {obj.email})")
+        self._dao.add(obj)
